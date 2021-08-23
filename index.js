abf56e7ee9b2d5f045607a1c019a07d617f32df0
Floyden <floydenmonteiro@gmail.com>
2021-08-23 16:57:06 +0530

    node js stream

---
+++ index.js
@@ -0,0 +1,36 @@
+const parse = require("csv-parse");
+const fs = require("fs");
+
+const habitablePlanet = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanet.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(habitablePlanet.map((planet) => {
+        return planet['kepler_name'];
+    }));
+    console.log(`${habitablePlanet.length} habitable planets  found!`);
+    console.log("done");
+  });
